38acf9557115e14fdabe006877967b5571678746
Masashi Yokota <bousou.tenshi6017@gmail.com>
2012-03-31 14:41:30 +0900

    Added write ro site bbs sql

---
+++ php/site/ro/sql/bbs.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `board` (
+  `board_id` int(11) NOT NULL AUTO_INCREMENT,
+  `board_name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `board_detail` text CHARACTER SET latin1 NOT NULL,
+  `creater` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `create_at` int(11) NOT NULL,
+  PRIMARY KEY (`board_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
+
+CREATE TABLE `board` (
+  `board_id` int(11) NOT NULL AUTO_INCREMENT,
+  `board_name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `board_detail` text CHARACTER SET latin1 NOT NULL,
+  `creater` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `create_at` int(11) NOT NULL,
+  PRIMARY KEY (`board_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
+
+CREATE TABLE `board` (
+  `board_id` int(11) NOT NULL AUTO_INCREMENT,
+  `board_name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `board_detail` text CHARACTER SET latin1 NOT NULL,
+  `creater` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `create_at` int(11) NOT NULL,
+  PRIMARY KEY (`board_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
