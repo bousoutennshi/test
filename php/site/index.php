2b82ee8f8f5937c6c8fce782326691c522e75ea4
Masashi Yokota <bousou.tenshi6017@gmail.com>
2012-03-31 14:34:17 +0900

    Added write site root in index.php

---
+++ php/site/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+        <title>myokota developments</title>
+    </head>
+    <body>
+        <div id="header">
+            <h3>myokota developments site</h3>
+        </div>
+
+        <div id="contents">
+            <ul>
+                <li><a href="http://www38143u.sakura.ne.jp/ro/yume.php">Ro Guild Site</a></li>
+                <li><a href="#">Video Site</a></li>
+                <li><a href="#">Word Press</a></li>
+            </ul>
+        </div>
+
+        <div id="footer">
+        </div>
+    </body>
+</html>
