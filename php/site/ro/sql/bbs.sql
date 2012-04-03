fadba942ce7d75f13315b487f27d8ba18c6b806c
myokota <myokota@www2228ue.sakura.ne.jp>
2012-04-03 21:37:38 +0900

    bug fix create table sql

--- php/site/ro/sql/bbs.sql
+++ php/site/ro/sql/bbs.sql
@@ -5,7 +5,7 @@ CREATE TABLE `board` (
   `creater` varchar(255) CHARACTER SET latin1 NOT NULL,
   `create_at` int(11) NOT NULL,
   PRIMARY KEY (`board_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user` (
   `user_id` varchar(255) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE `user` (
   `passwd` varchar(255) NOT NULL,
   `mail` text,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `message` (
   `serial_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -22,4 +22,4 @@ CREATE TABLE `message` (
   `poster` varchar(255) NOT NULL,
   `message` text NOT NULL,
   PRIMARY KEY (`serial_id`,`board_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
